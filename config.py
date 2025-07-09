7d5b73b38023f23788eccb88190dc98270eb9c39
pratap9155 <venompratap22@gmail.com>
2025-07-09 15:47:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,16 +7,16 @@ from pyrogram import filters
 load_dotenv()
 # ------------------------------------
 # -----------------------------------------------------
-API_ID = int(getenv("API_ID", "26788076"))
-API_HASH = getenv("API_HASH", "5f6d551ff94c49365428978a182facb4")
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH", "")
 
 EVAL = list(map(int, getenv("EVAL", "7499773246 1073815732").split()))
 # ------------------------------------------------------
-BOT_TOKEN = getenv("BOT_TOKEN", "7214950972:AAFpMkZYMIKFQrmtg2hU_c5h8FNJWTdv3b0")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
 # -------------------------------------------------------
-OWNER_USERNAME = getenv("OWNER_USERNAME","Ownergit")
+OWNER_USERNAME = getenv("OWNER_USERNAME","")
 # --------------------------------------------------------
-BOT_USERNAME = getenv("BOT_USERNAME" , "ISTKHARkoBot")
+BOT_USERNAME = getenv("BOT_USERNAME" , "")
 # --------------------------------------------------------
 BOT_NAME = getenv("BOT_NAME" , "ISTKHARko")
 # ---------------------------------------------------------
@@ -24,7 +24,7 @@ ASSUSERNAME = getenv("ASSUSERNAME" , "MissYumikoo")
 # ---------------------------------------------------------
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://venompratap:venom9155@cluster0.dcz5eaa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MONGO_DB_URI = getenv("MONGO_DB_URI", "")
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
 # ----------------------------------------------------------------
@@ -45,7 +45,7 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # -----------------------------------------
 API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
-API_KEY = getenv("API_KEY", 'NxGBNexGenBots0688b3') # youtube song api ke
+API_KEY = getenv("API_KEY", ) # youtube song api ke
 # -------------------------------------------------------------------------
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/venompratap")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/venompratapchat")
@@ -64,7 +64,7 @@ TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", "5242880000"))
 TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", "5242880000"))
 # ------------------------------------
 # ------------------------------------
-STRING1 = getenv("STRING_SESSION", "BQGYwOwAs5bb1_BIPwq72jPbhYq4_jpwCcpimG96O91bWd07Km7wedurcOWSLLjAdr-uC8S-NLyxJIyhpTAjRlzt7jPjMKt3axHgvhN_egWRLesPSyOygZvgSvFT-iicnIDSKhzjtIUtvivaNO4ysRtfQF8oYV8nVe0Pf1qMrn0GLO_j1kUXrFZv1uQRCtJSLksBOmzE0iL2bFke3NTRAoD49oiJrXicthIAWGaXbOjgfM2SnfiBHWMIM7mDFK5TSz_P8u0je3GDxlVoVPjC3dLYXOBVmyPjyOA5n7PL7l5F0x4HpeSBCO_TT_HzOCuVcRlirBS104Z1aN6dT8OnmDzt01GYsAAAAAHEXmh9AA")
+STRING1 = getenv("STRING_SESSION", "")
 STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
 STRING4 = getenv("STRING_SESSION4", None)
